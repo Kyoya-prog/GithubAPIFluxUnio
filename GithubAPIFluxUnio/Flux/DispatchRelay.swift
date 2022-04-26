0be4ee0e4e45418e45aaddbe4da3fd46b30223f2
kyoya <kyoyaprog@gmail.com>
2022-04-27 00:05:00 +0900

    DispatchRelayの実装

---
+++ GithubAPIFluxUnio/Flux/DispatchRelay.swift
@@ -0,0 +1,17 @@
+import RxRelay
+import RxSwift
+
+final class DispatchRelay<Element>{
+    private let relay = PublishRelay<Element>()
+    
+    func dispatch(_ element:Element){
+        relay.accept(element)
+    }
+}
+
+extension DispatchRelay: ObservableType {
+    func subscribe<Observer: ObserverType>(_ observer: Observer) -> Disposable
+        where Element == Observer.Element {
+        return relay.subscribe(observer)
+    }
+}
