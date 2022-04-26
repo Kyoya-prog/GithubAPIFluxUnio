b473f258d6ce7b385f30c5fc2a477d1312f8e7a8
kyoya <kyoyaprog@gmail.com>
2022-04-26 21:45:46 +0900

    Initial Commit

---
+++ GithubAPIFluxUnio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAPIFluxUnio
+//
+//  Created by 松山響也 on 2022/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
